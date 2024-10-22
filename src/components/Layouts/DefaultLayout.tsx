9b8683f5d250cc04bcb2f44f7bdb216e8f07ba13
maritzarafa <58309017+Apuy717@users.noreply.github.com>
2024-10-22 16:29:39 +0700

    first commit

---
+++ src/components/Layouts/DefaultLayout.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const pathname = usePathname();
+  return (
+    <>
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex">
+        {/* <!-- ===== Sidebar Start ===== --> */}
+        {!pathname.includes("/auth") && (
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        )}
+
+        {/* <!-- ===== Sidebar End ===== --> */}
+
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div
+          className={`"relative flex flex-1 flex-col ${!pathname.includes("/auth") && "lg:ml-72.5"}`}
+        >
+          {/* <!-- ===== Header Start ===== --> */}
+          {!pathname.includes("/auth") && (
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          )}
+
+          {/* <!-- ===== Header End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </>
+  );
+}
