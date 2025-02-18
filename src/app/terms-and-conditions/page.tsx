c5c8d078bd56a0bba0ba483e506deeaf652a6c7d
sholichan <sholichan16@gmail.com>
2025-02-18 22:13:24 +0700

    TNC module

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -5,6 +5,7 @@ import {
   Input,
   InputDropdown,
   InputTextArea,
+  InputTextAreaWithKeydown,
 } from "@/components/Inputs/InputComponent";
 import { GetWithToken, PostWithToken } from "@/libs/FetchData";
 import { RootState } from "@/stores/store";
@@ -14,11 +15,18 @@ import { useRouter } from "next/navigation";
 import * as Yup from "yup";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Link from "next/link";
+import { ERoles } from "@/stores/authReducer";
+import Table from "@/components/Tables/Table";
+import { TNC } from "@/types/tnc";
+import { FiEdit, FiEye } from "react-icons/fi";
+import { FaArrowLeft } from "react-icons/fa";
 
 interface MyResponse {
   statusCode: number;
   msg: string;
   data: any;
+  // total: number;
   err: string | string[];
 }
 
@@ -26,228 +34,159 @@ export default function TermsAndConditions() {
   const auth = useSelector((s: RootState) => s.auth);
   const router = useRouter();
   const [outlets, setOutlets] = useState<iDropdown[]>([]);
+  const [terms, setTerms] = useState<TNC[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    const GotOutlets = async () => {
-      let urlwithQuery = `/api/outlet`;
-      const res = await GetWithToken<MyResponse>({
-        router: router,
-        url: urlwithQuery,
-        token: `${auth.auth.access_token}`,
-      });
-      const allOutlet = {
-        label: "All",
-        value: "all",
-      };
-      const mapingOutlet = res.data.map((i: any) => {
-        return {
-          label: i.name,
-          value: i.id,
-        };
-      });
-      mapingOutlet.unshift(allOutlet);
-      console.log(mapingOutlet);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalTerms, setTotalTerms] = useState(0);
+  const [isViewDetail, setIsViewDetail] = useState<boolean>(false);
+  const [idx, setIdx] = useState<number>(0);
 
-      if (mapingOutlet.length >= 1) {
-        setOutlets(mapingOutlet);
-      }
-    };
-    GotOutlets();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const formik = useFormik({
-    initialValues: {
-      outlet_id: "all",
-      title: "",
-      items: [
-        {
-          label: "",
-          text: "",
-        },
-      ],
-    },
-    validationSchema: Yup.object({
-      outlet_id: Yup.string().required(),
-      title: Yup.string().required(),
-      items: Yup.array().of(
-        Yup.object({
-          label: Yup.string(),
-          text: Yup.string(),
-        }),
-      ),
-    }),
-    onSubmit: async (values) => {
-      if (loading) return;
-      setLoading(true);
-      const updatedValues = {
-        ...values,
-        outlet_id: values.outlet_id === "all" ? null : values.outlet_id,
+  useEffect(() => {
+    const GotTerms = async () => {
+      const data = {
+        outlet_ids: [],
       };
+      let urlwithQuery = `/api/t-and-c?${currentPage}&limit=${10}`;
       const res = await PostWithToken<MyResponse>({
         router: router,
-        url: "/api/t-and-c/create-update",
-        data: updatedValues,
+        url: urlwithQuery,
+        data: data,
         token: `${auth.auth.access_token}`,
       });
-      if (res.statusCode === 422) {
-        (res.err as string[]).map((i) => {
-          const field = i.split(" ");
-          if (field.length >= 1) formik.setFieldError(field[0], i);
-        });
-      }
 
       if (res.statusCode === 200) {
-        toast.success("Success create Terms and Conditions!");
-        router.push("/");
+        setTerms(res.data);
         console.log(res.data);
       }
-      setLoading(false);
-    },
-  });
+    };
+    GotTerms();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const addVariant = () => {
-    formik.setFieldValue("items", [
-      ...formik.values.items,
-      {
-        label: "",
-        text: "",
-      },
-    ]);
-  };
-
-  const removeVariant = (index: any) => {
-    const items = [...formik.values.items];
-    items.splice(index, 1);
-    formik.setFieldValue("items", items);
-  };
   return (
     <div>
-      <Breadcrumb pageName="Terms and Conditions" />
-      <div className="h-full w-full space-y-6 rounded-md bg-white p-4 dark:bg-boxdark">
-        <InputDropdown
-          label={"Outlets*"}
-          name={"Outlets"}
-          id={"Outlets"}
-          value={
-            formik.values.outlet_id === ""
-              ? outlets.length < 1
-                ? ""
-                : outlets[0].value
-              : formik.values.outlet_id
-          }
-          onChange={(v) => formik.setFieldValue(`outlet_id`, v)}
-          options={outlets}
-          error={
-            formik.touched.outlet_id && formik.errors.outlet_id
-              ? formik.errors.outlet_id
-              : null
-          }
-        />
-        <Input
-          label={"Terms and Conditions Title*"}
-          name={"title"}
-          id={"title"}
-          value={formik.values.title}
-          onChange={(v) => formik.setFieldValue("title", v)}
-          error={
-            formik.touched.title && formik.errors.title
-              ? formik.errors.title
-              : null
-          }
-        />
-        {formik.values.items.map((i, index) => (
-          <div key={index}>
-            <div className="h-full w-full space-y-6 rounded-lg p-4 outline outline-1 outline-slate-400">
-              <div className="flex justify-between">
-                <p className="font-bold">Items {index + 1}</p>
-                <div className="">
-                  <button
-                    className={
-                      index == 0
-                        ? `hidden`
-                        : `rounded bg-red-700 p-2 text-sm text-white`
-                    }
-                    onClick={() => removeVariant(index)}
-                  >
-                    Delete items
-                  </button>
-                </div>
-              </div>
-              <Input
-                label={"Items Title*"}
-                name={"label"}
-                id={"label"}
-                value={i.label}
-                onChange={(v) =>
-                  formik.setFieldValue(`items[${index}].label`, v)
-                }
-                error={
-                  formik.touched.items?.[index]?.label &&
-                  typeof formik.errors.items?.[index] === "object" &&
-                  formik.errors.items[index]?.label
-                    ? formik.errors.items[index].label
-                    : null
-                }
-              />
-              <InputTextArea
-                label={"Items Content*"}
-                name={"text"}
-                id={"text"}
-                value={i.text}
-                onChange={(v) =>
-                  formik.setFieldValue(`items[${index}].text`, v)
-                }
-                error={
-                  formik.touched.items?.[index]?.text &&
-                  typeof formik.errors.items?.[index] === "object" &&
-                  formik.errors.items[index]?.text
-                    ? formik.errors.items[index].text
-                    : null
-                }
-              />
-            </div>
-          </div>
-        ))}
-
-        <div className="w-full">
-          <button
-            onClick={addVariant}
-            className="w-auto rounded-md bg-blue-500 px-10 py-2 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+      <div className="mb-4 w-full  rounded-t bg-white p-4 dark:bg-boxdark">
+        <div className="flex w-full flex-col space-y-6 md:flex-row md:space-x-4 md:space-y-0">
+          <Link
+            href={"/terms-and-conditions/create"}
+            className={`${auth.role.name !== ERoles.PROVIDER && auth.role.name !== ERoles.SUPER_ADMIN && "hidden"}  inline-flex items-center 
+            justify-center rounded-md bg-black px-10 py-3 text-center font-medium text-white hover:bg-opacity-90
+            dark:text-gray-400 lg:px-8 xl:px-10`}
           >
-            Add items
-          </button>
-        </div>
-        <div className="grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-1">
-          <button
-            onClick={formik.submitForm}
-            className="inline-flex items-center justify-center rounded-md bg-black px-10 py-2 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-          >
-            Submit
-          </button>
+            Create T&C
+          </Link>
         </div>
       </div>
 
-      <div className="mt-6 h-full w-full space-y-6 rounded-md bg-white p-4 dark:bg-boxdark">
-        <div className="w-full">
-          <button
-            onClick={() => {}}
-            className="w-auto rounded-md bg-gray-500 px-10 py-2 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+      <Table
+        colls={["TITLE", "CREATED AT", "LAST UPDATED", "ACTION"]}
+        onPaginate={(page) => setCurrentPage(page)}
+        currentPage={0}
+        totalItem={0}
+      >
+        {terms?.map((i, index) => (
+          <tr
+            key={index}
+            className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
           >
-            Preview
+            <td className="px-6 py-4">{i.title}</td>
+            <td className="px-6 py-4">
+              {new Date(i.created_at).toLocaleDateString("id", {
+                day: "2-digit",
+                month: "short",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              })}
+            </td>
+            <td className="px-6 py-4">
+              {new Date(i.updated_at).toLocaleDateString("id", {
+                day: "2-digit",
+                month: "short",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              })}
+            </td>
+            <td className="px-6 py-4">
+              <div className=" flex flex-row items-center space-x-2">
+                <div className="group relative">
+                  <button
+                    className="cursor-pointer"
+                    onClick={() => {
+                      setIdx(index);
+                      setIsViewDetail(true);
+                    }}
+                  >
+                    <FiEye size={18} />
+                  </button>
+                  <div className="absolute bottom-[70%] mb-2 hidden -translate-x-1/2 transform rounded-md bg-gray-800 px-2 py-1 text-xs text-white opacity-85 group-hover:block">
+                    View detail
+                  </div>
+                </div>
+                <div className="group relative">
+                  <button
+                    onClick={() => {
+                      router.push(`terms-and-conditions/update/${i.outlet_id}`);
+                    }}
+                  >
+                    <FiEdit size={18} />
+                  </button>
+                  <div className="absolute bottom-[70%] mb-2 hidden -translate-x-1/2 transform rounded-md bg-gray-800 px-2 py-1 text-xs text-white opacity-85 group-hover:block">
+                    Edit T&C
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </Table>
+
+      <div
+        className={`fixed right-0 top-0 z-[9999] h-full w-[80%] overflow-x-auto overflow-y-auto
+              bg-white shadow transition-all duration-500 dark:bg-boxdark
+              ${isViewDetail ? "" : "translate-x-full"}`}
+      >
+        <div className="bg-white p-4 shadow dark:bg-boxdark">
+          <button
+            onClick={() => {
+              setIsViewDetail(false);
+            }}
+          >
+            <FaArrowLeft size={20} className="rotate-180" />
           </button>
         </div>
-        <div className="h-full w-full space-y-6 rounded-lg p-4 outline outline-1 outline-slate-400 text-black-2  dark:text-gray-300">
-          <p className="text-3xl font-bold">{formik.values.title}</p>
-          {formik.values.items.map((i, index) => (
-            <div key={index}>
-              <p className="text-xl font-bold">
-                {index + 1 +'. '+ formik.values.items[index].label}
-              </p>
-              <p className="">
-                {formik.values.items[index].text}
-              </p>
+        <div className="mt-4 p-4">
+          <div className="mt-6 h-full w-full space-y-6 rounded-md bg-white p-4 dark:bg-boxdark">
+            {/* <div className="w-full">
+              <button
+                onClick={() => {}}
+                className="w-auto rounded-md bg-gray-500 px-10 py-2 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+              >
+                Preview
+              </button>
+            </div> */}
+            <div className="h-full w-full space-y-6 rounded-lg p-4 text-black-2 outline outline-1 outline-slate-400  dark:text-gray-300">
+              <p className="text-3xl font-bold">{terms[idx]?.title}</p>
+              {terms[idx]?.terms_and_conditions_items?.map((i, index) => (
+                <div key={index}>
+                  <p className="text-xl font-bold">
+                    {index + 1 + ". " + i.label}
+                  </p>
+                  <p className="">
+                    {i.text.split("\n").map((line, index) => (
+                      <span key={index}>
+                        {line}
+                        <br />
+                      </span>
+                    ))}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
