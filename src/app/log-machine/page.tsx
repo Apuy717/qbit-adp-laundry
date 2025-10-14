66f0930fd58fe01623b4b62b770bbbfd1f9a590a
Apuy717 <58309017+Apuy717@users.noreply.github.com>
2025-10-14 14:12:48 +0700

    display turn

--- src/app/log-machine/page.tsx
+++ src/app/log-machine/page.tsx
@@ -155,6 +155,7 @@ export default function LogMachine() {
           "machine",
           "status",
           "access by",
+          "Turn off by",
           "start at",
           "end at",
           "target",
@@ -182,6 +183,7 @@ export default function LogMachine() {
             <td className="whitespace-nowrap px-6 py-4">{i.machine.name}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.status == "maintance" ? "MAINTENANCE" : i.status}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.accessed_by}</td>
+            <td className="whitespace-nowrap px-6 py-4 uppercase">{i.turn_off_by_system ? "Otomatis" : "Manual"}</td>
             <td className="whitespace-nowrap px-6 py-4">{new Date(i.created_at).toLocaleDateString("id", {
               day: "2-digit",
               month: "short",
