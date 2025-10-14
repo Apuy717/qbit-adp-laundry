0ebed7412f581956cbb581156cbea2958c2949a4
Apuy717 <58309017+Apuy717@users.noreply.github.com>
2025-10-14 14:15:25 +0700

    fixing if in log machine

--- src/app/log-machine/page.tsx
+++ src/app/log-machine/page.tsx
@@ -183,7 +183,7 @@ export default function LogMachine() {
             <td className="whitespace-nowrap px-6 py-4">{i.machine.name}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.status == "maintance" ? "MAINTENANCE" : i.status}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.accessed_by}</td>
-            <td className="whitespace-nowrap px-6 py-4 uppercase">{i.turn_off_by_system ? "Otomatis" : "Manual"}</td>
+            <td className="whitespace-nowrap px-6 py-4 uppercase"> {i.status === "finished" ? i.turn_off_by_system ? "Otomatis" : "Manual" : "-"}</td>
             <td className="whitespace-nowrap px-6 py-4">{new Date(i.created_at).toLocaleDateString("id", {
               day: "2-digit",
               month: "short",
