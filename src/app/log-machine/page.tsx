b9c258279fafd3f55a5a458917b580ef5c2454b2
Apuy717 <58309017+Apuy717@users.noreply.github.com>
2025-10-14 14:23:49 +0700

    display status mesin on or off

--- src/app/log-machine/page.tsx
+++ src/app/log-machine/page.tsx
@@ -155,6 +155,7 @@ export default function LogMachine() {
           "machine",
           "status",
           "access by",
+          "Status Machine",
           "Turn off by",
           "start at",
           "end at",
@@ -183,6 +184,7 @@ export default function LogMachine() {
             <td className="whitespace-nowrap px-6 py-4">{i.machine.name}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.status == "maintance" ? "MAINTENANCE" : i.status}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase">{i.accessed_by}</td>
+            <td className="whitespace-nowrap px-6 py-4 uppercase">{i.order_item_stage.status}</td>
             <td className="whitespace-nowrap px-6 py-4 uppercase"> {i.order_item_stage.status === "finished" ? i.turn_off_by_system ? "Otomatis" : "Manual" : "-"}</td>
             <td className="whitespace-nowrap px-6 py-4">{new Date(i.created_at).toLocaleDateString("id", {
               day: "2-digit",
